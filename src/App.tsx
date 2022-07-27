07ff8c7568a4a7397dce40e34ca2dc7862cf8ab6
gustavo <gustavo.felipebc@gmail.com>
2022-07-27 17:54:14 -0300

    configuring creation modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global"
 
+//ACESSIBILIDADE: repasso ao Modal onde está o root para que os leitores de tela por exemplo informem
+//ao usuário que a #root não está passível de interação quando o modal está ativo 
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
